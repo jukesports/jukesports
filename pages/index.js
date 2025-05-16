f9611b52fdc4e0b4c54f9d4b2bc7e6e59b75d1c8
jukesports <owen@atompayments.co>
2025-05-16 19:53:02 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -41,3 +41,46 @@ const games = [
 ];
 
 export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>JukeSports - Live Lacrosse Scores</title>
+        <meta name="description" content="Live NCAA and PLL Lacrosse Scores" />
+      </Head>
+
+      <header className="header">
+        <img src="/logos/jukesports_logo.png" alt="JukeSports Logo" className="logo" />
+        <h1 className="title">JukeSports</h1>
+        <nav className="nav">
+          <a href="#">NCAA</a>
+          <a href="#">PLL</a>
+          <a href="#">NLL</a>
+        </nav>
+      </header>
+
+      <main className="grid">
+        {games.map((game, index) => (
+          <div key={index} className="game-card">
+            <div className="game-header">
+              <div className="team">
+                {game.team1Logo && <img src={game.team1Logo} alt={game.team1} className="team-logo" />}
+                <span>{game.team1}</span>
+              </div>
+              <div className="score">{game.score}</div>
+              <div className="team">
+                {game.team2Logo && <img src={game.team2Logo} alt={game.team2} className="team-logo" />}
+                <span>{game.team2}</span>
+              </div>
+            </div>
+            <div className="network">Network: {game.network}</div>
+            <blockquote className="twitter-tweet">
+              <a href={`https://twitter.com/i/status/${game.tweetId}`}></a>
+            </blockquote>
+          </div>
+        ))}
+      </main>
+
+      <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
+    </div>
+  );
+}
